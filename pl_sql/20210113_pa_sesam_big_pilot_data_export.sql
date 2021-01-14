1483161fdd7b128d75abdc093d061bdab37ae399
Peter von Rohr <peter.vonrohr@qualitasag.ch>
2021-01-14 07:28:29 +0100

    Added first script that does the complete export of all farms in the big pilot

---
+++ pl_sql/20210113_pa_sesam_big_pilot_data_export.sql
@@ -0,0 +1,27 @@
+CONNECT GESTHO/GESTHO@ARGUS;
+-- 
+--
+--
+declare
+  type btr_array_t is table of hfaexpl.idexploitation%TYPE index by pls_integer;
+  a_btr_ids   btr_array_t;
+  nidx   pls_integer;
+begin
+  a_btr_ids(0) := 10030720;
+  a_btr_ids(1) := 4370090;
+  a_btr_ids(2) := 965076437;
+  a_btr_ids(3) := 4650380;
+  a_btr_ids(4) := 965071204;
+  -- loop over a_btr_ids
+  nidx := a_btr_ids.first;
+  dbms_output.put_line(' * Starting Sesam Data Export ...');
+  while nidx is not NULL
+  loop
+    dbms_output.put_line(' * Sesam Data Export for farm: ' || a_btr_ids(nidx));
+    pa_sesam.ExportSesamData(pnIdExpl => a_btr_ids(nidx));
+    nidx := a_btr_ids.next(nidx);
+  end loop;
+end;
+/
+SHOW ERRORS;
+EXIT;
