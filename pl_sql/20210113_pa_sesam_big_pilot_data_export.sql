c456f6f212a5e58fbf4a72d89c8c99dd7ee849dd
Peter von Rohr <peter.vonrohr@qualitasag.ch>
2021-01-14 07:32:51 +0100

    Added addresses for farms of big pilot

--- pl_sql/20210113_pa_sesam_big_pilot_data_export.sql
+++ pl_sql/20210113_pa_sesam_big_pilot_data_export.sql
@@ -7,11 +7,11 @@ declare
   a_btr_ids   btr_array_t;
   nidx   pls_integer;
 begin
-  a_btr_ids(0) := 10030720;
-  a_btr_ids(1) := 4370090;
-  a_btr_ids(2) := 965076437;
-  a_btr_ids(3) := 4650380;
-  a_btr_ids(4) := 965071204;
+  a_btr_ids(0) := 10030720;    -- 1003072 Tscharner Risch, Sax 20, 7492 Alvaneu Dorf
+  a_btr_ids(1) := 4370090;     -- 347100 Duschletta Curdin, Scheschna, 7530 Zernez
+  a_btr_ids(2) := 965076437;   -- 340098 Bucheli-Caduff Guido, Via Salez 102, 7186 Segnas
+  a_btr_ids(3) := 4650380;     -- 331104 Walser-Roffler Andreas, Sänder 1, 7023 Haldenstein
+  a_btr_ids(4) := 965071204;   -- 347086 Koller August, Bröl 55, 7546 Ardez
   -- loop over a_btr_ids
   nidx := a_btr_ids.first;
   dbms_output.put_line(' * Starting Sesam Data Export ...');
